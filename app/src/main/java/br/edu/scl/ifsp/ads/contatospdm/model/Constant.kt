1e4d47fdefeeff95456902068cd26635f8295538
AlexandreSCavaleiro <alexandre_cavaleiro@outlook.com>
2023-10-03 20:46:36 -0300

    DAO criado e criando tabela

--- app/src/main/java/br/edu/scl/ifsp/ads/contatospdm/model/Constant.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/contatospdm/model/Constant.kt
@@ -3,4 +3,5 @@ package br.edu.scl.ifsp.ads.contatospdm.model
 object Constant {
     const val EXTRA_CONTACT = "EXTRA_CONTACT"
     const val VIEW_CONTACT = "VIEW_CONTACT"
+    const val IINVALID_CONTACT_ID = -1
 }
